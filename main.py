096b32ddc464ea1918dd69bbe9739df4a971839e
arav0917 <auvyawahare@zonemail.clpccd.edu>
2023-06-23 14:26:08 -0700

    FIRST

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 def main():
-    """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celcius fahrenheit 
-    ##################################################
-    """
+    celsius = int(input("Enter the temp in degrees celsius: "))
+    fahrenheit = celsius*9/5 + 32
+    print(f"fahrenheit: \t {fahrenheit:.2f}")
 
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return celcius, fahrenheit
+    return celsius, fahrenheit
 
 
 if __name__ == '__main__':
